89d5721e2d1cabac51bd886c1afefe5f20fc4c58
Reynir Björnsson <rbj@isnordic.dk>
2017-04-15 15:54:01 +0200

    Lookup in the environment
    Documentation will show up for items from opened modules, too.
    
    Fixes #2

--- ocpIndexTop.ml
+++ ocpIndexTop.ml
@@ -24,12 +24,41 @@ let lib_dirs = stdlib_dir :: opamlib_dirs
 
 let index = LibIndex.load lib_dirs
 
+let mk_resolver find lident =
+  let env = !Toploop.toplevel_env in
+    try
+      let path = find lident env in
+      Some (Path.name path)
+    with Not_found ->
+      None
+
+let resolvers = [
+  mk_resolver (fun lident env -> fst (Env.lookup_type lident env));
+  mk_resolver (fun lident env -> fst (Env.lookup_value lident env));
+  mk_resolver (Env.lookup_module ~load:true);
+  mk_resolver (fun lident env -> fst (Env.lookup_modtype lident env));
+  mk_resolver (fun lident env -> fst (Env.lookup_class lident env));
+]
+
+let resolve_all : (Longident.t -> string option) list -> Longident.t -> string =
+  fun fs lident ->
+    match List.fold_right
+            (fun f acc ->
+               match acc with
+               | Some _ -> acc
+               | None -> f lident)
+            fs
+            None
+    with
+    | Some s -> s
+    | None -> Longident.flatten lident |> String.concat "."
+
 let () =
   Hashtbl.add
     Toploop.directive_table
     "doc"
     (Toploop.Directive_ident (fun lident ->
-         let s = Longident.flatten lident |> String.concat "." in
+         let s = resolve_all resolvers lident in
          match Lazy.force (LibIndex.get index s).LibIndex.doc with
          | Some doc ->
            print_endline doc
