40a89ace4af8bf0d607da38d8d7c1d187c22432c
Reynir Björnsson <rbj@isnordic.dk>
2017-04-27 21:29:20 +0200

    Derp, actually build something
    It's broken

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -4,7 +4,7 @@
 open Topkg
 
 let cmd c os files =
-  OS.Cmd.run Cmd.(Pkg.build_cmd c os %% v "-plugin-tag" %% v "package(cppo_ocamlbuild)")
+  OS.Cmd.run Cmd.(Pkg.build_cmd c os %% v "-plugin-tag" %% v "package(cppo_ocamlbuild)" %% of_list files)
 
 let () =
   Pkg.describe "ocp-index-top" ~build:(Pkg.build ~cmd ()) @@ fun c ->
