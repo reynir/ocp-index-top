af0ebbae18b53ff54150856350957e2118406b8e
Reynir Björnsson <rbj@isnordic.dk>
2017-04-27 21:08:20 +0200

    Add conditional compilation
    Hopefully this fixed #7 and #6

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -3,8 +3,11 @@
 #require "topkg"
 open Topkg
 
+let cmd c os files =
+  OS.Cmd.run Cmd.(Pkg.build_cmd c os %% v "-plugin-tag" %% v "package(cppo_ocamlbuild)")
+
 let () =
-  Pkg.describe "ocp-index-top" @@ fun c ->
+  Pkg.describe "ocp-index-top" ~build:(Pkg.build ~cmd ()) @@ fun c ->
   Ok [
     Pkg.mllib ~api:[] "ocp-index-top.mllib";
   ]
